2dfdc86ac10f81ea4e61c2772f12d5e98541fe3b
Daniel Gomez <danigb66@gmail.com>
2024-11-21 16:14:32 -0500

    First commit

---
+++ src/products/products.controller.ts
@@ -0,0 +1,62 @@
+import { Body, Controller, Delete, Get, Inject, Param, ParseIntPipe, Patch, Post, Query } from '@nestjs/common';
+import { ClientProxy, RpcException } from '@nestjs/microservices';
+import { catchError, firstValueFrom } from 'rxjs';
+import { PaginationDto } from 'src/common';
+import { PRODUCT_SERVICES } from 'src/config';
+import { CreateProductDto } from './dto/create-product.dto';
+import { UpdateProductDto } from './dto/update-product.dto';
+
+@Controller('products')
+export class ProductsController {
+  constructor(
+    @Inject(PRODUCT_SERVICES) private readonly productsClient: ClientProxy
+  ) {}
+
+  @Post()
+  createProduct(@Body() createProductDto: CreateProductDto) {
+    return this.productsClient.send({ cmd: 'create_product' }, createProductDto)
+  }
+
+  @Get()
+  findAllProducts(@Query() paginationDto: PaginationDto) {
+    return this.productsClient.send({ cmd: 'find_all_products' }, paginationDto)
+  }
+
+  @Get(':id')
+  async findOneProduct(@Param('id') id: string) {
+    try {
+      const product = await firstValueFrom(
+        this.productsClient.send({ cmd: 'find_one_product' }, { id })
+      )
+      return product;
+    } catch (error) {
+      throw new RpcException(error);
+    }
+  }
+
+  @Delete(':id')
+  deleteProduct(@Param('id') id: string) {
+    // Esta es otra forma de hacerlo en vez del try y catch para el manejo de errores y excepciones
+    return this.productsClient.send({ cmd: 'delete_product' }, { id }).pipe(
+      catchError( err => { throw new RpcException(err) })
+    )
+  }
+
+  @Patch(':id')
+  async updateProduct(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() updateProductDto: UpdateProductDto
+  ) {
+    try {
+      const product = await firstValueFrom(
+        this.productsClient.send({ cmd: 'update_product' }, {
+          id,
+          ...updateProductDto
+        })
+      )
+      return product;
+    } catch (error) {
+      throw new RpcException(error);
+    }
+  }
+}
