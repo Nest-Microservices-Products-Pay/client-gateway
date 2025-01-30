e8d4b241fc09deea64fec786bb73c95478ba4c81
Daniel Gomez <danigb66@gmail.com>
2025-01-30 14:18:02 -0500

    Actualizacion NAST

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -2,15 +2,15 @@ import { Body, Controller, Delete, Get, Inject, Param, ParseIntPipe, Patch, Post
 import { ClientProxy, RpcException } from '@nestjs/microservices';
 import { catchError, firstValueFrom } from 'rxjs';
 import { PaginationDto } from 'src/common';
-import { PRODUCT_SERVICES } from 'src/config';
+import { NATS_SERVICE } from 'src/config';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 
 @Controller('products')
 export class ProductsController {
   constructor(
-    @Inject(PRODUCT_SERVICES) private readonly productsClient: ClientProxy
-  ) {}
+    @Inject(NATS_SERVICE) private readonly productsClient: ClientProxy
+  ) { }
 
   @Post()
   createProduct(@Body() createProductDto: CreateProductDto) {
@@ -38,7 +38,7 @@ export class ProductsController {
   deleteProduct(@Param('id') id: string) {
     // Esta es otra forma de hacerlo en vez del try y catch para el manejo de errores y excepciones
     return this.productsClient.send({ cmd: 'delete_product' }, { id }).pipe(
-      catchError( err => { throw new RpcException(err) })
+      catchError(err => { throw new RpcException(err) })
     )
   }
 
