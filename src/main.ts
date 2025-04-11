57bbb8dad7518dd8f58f0c8273b4e71a7aefdf1f
Daniel Gomez <danigb66@gmail.com>
2025-04-11 09:19:51 -0500

    Add health check point

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { envs } from './config';
-import { Logger, ValidationPipe } from '@nestjs/common';
+import { Logger, RequestMethod, ValidationPipe } from '@nestjs/common';
 import { RpcCustomExceptionFilter } from './common';
 
 async function bootstrap() {
   const logger = new Logger('Main');
   const app = await NestFactory.create(AppModule);
 
-  app.setGlobalPrefix('api');
+  app.setGlobalPrefix('api', {
+    exclude: [{
+      path: '',
+      method: RequestMethod.GET
+    }]
+  });
 
   app.useGlobalPipes(
     new ValidationPipe({
@@ -21,7 +26,7 @@ async function bootstrap() {
 
   await app.listen(envs.port);
 
-  console.log('Hola desde client gateway ms')
+  console.log('Health check configured')
 
   logger.log(`Gateway Running on port ${envs.port}`);
 }
